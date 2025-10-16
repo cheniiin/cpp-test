44de51c8098ff7e83d05c12dd142658a316b9ea1
cheniiin <s1120348@dmhs.kh.edu.tw>
2025-10-16 14:40:13 +0800

    作業5

--- test1/main.cpp
+++ test1/main.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 
 int main() {
-    int sum = 0;  // 雖然宣告了 sum，但沒有用到
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0)
-            cout << i << " ";
+    // Printing star pattern using for loops
+    cout << "Star Pattern: \n";
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
     }
+
+    // Printing multiplication table using while loops
+    cout << "\nMultiplication Table: \n";
+    int i = 1;
+    while (i <= 9) {
+        int j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
     return 0;
 }
