a734333c094369f1b4c0472faa0aef80dacaa780
cheniiin <s1120348@dmhs.kh.edu.tw>
2025-11-27 14:36:53 +0800

    作業10

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <cstdlib>   // rand(), srand()
+#include <ctime>     // time()
+
 using namespace std;
 
 int main() {
-    int number = 30;
-    cout << "The value of number is: " << number << endl;
-    return 0;
-}
+    const int N = 10;   // 陣列大小
+    int arr[N];
 
-int sum(int a, int b) {
-    return a + b;
-}
+    // --- 隨機產生 ---
+    srand(time(NULL));
+
+    cout << "原始亂數陣列：";
+    for (int i = 0; i < N; i++) {
+        arr[i] = rand() % 100;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // --- 選擇排序 (Selection Sort) ---
+    for (int i = 0; i < N - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < N; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
